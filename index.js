968ad703414567e0756e0ee5a3198f2b1791a769
sdshuvo98 <hellosdshuvo@gmail.com>
2022-04-29 00:08:29 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
+
 
 //middleWare
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y9ynz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const itemCollection = client.db("autoMent").collection("items");
+    // perform actions on the collection object
+    console.log("connected")
+    client.close();
+});
+
 
 app.get('/', (req, res) => {
     res.send("AutoMent Server Home")
