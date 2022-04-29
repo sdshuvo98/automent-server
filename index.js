4aa57a466f2032be7fdad1ffae8a2d60ec5937f6
sdshuvo98 <hellosdshuvo@gmail.com>
2022-04-29 13:30:27 +0600

    get item by id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -13,13 +13,31 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y9ynz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const itemCollection = client.db("autoMent").collection("items");
-    // perform actions on the collection object
-    console.log("connected")
-    client.close();
-});
+async function run() {
+    try {
+        await client.connect();
+        const itemCollection = client.db("autoMent").collection("items");
 
+        // GET Items
+        app.get('/items', async (req, res) => {
+            const cursor = itemCollection.find()
+            const items = await cursor.toArray()
+            res.send(items)
+        })
+        app.get('/items/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await itemCollection.findOne(query)
+            res.send(item)
+        })
+
+
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send("AutoMent Server Home")
