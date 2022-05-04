d8af945cb653e9f8a44ae973fb669c3e3933e4a5
sdshuvo98 <hellosdshuvo@gmail.com>
2022-05-04 15:42:34 +0600

    test removed

--- index.js
+++ index.js
@@ -80,9 +80,6 @@ async function run() {
             const item = await itemCollection.findOne(query)
             res.send(item)
         })
-        app.get('/test', async (req, res) => {
-            res.send('this is test')
-        })
 
         // Auth
         app.post('/login', async (req, res) => {
